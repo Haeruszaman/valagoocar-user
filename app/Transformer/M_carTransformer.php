520fac1bf069ceeab6612102a81fd1635c71e4eb
unknown <haeruszaman53@gmail.com>
2018-06-10 20:26:53 +0700

    Updated M_carTransformer

--- app/Transformer/M_carTransformer.php
+++ app/Transformer/M_carTransformer.php
@@ -8,11 +8,11 @@ class M_carTransformer extends TransformerAbstract
     public function transform(M_car $M_car) {
 
         return [
-            'name' => $M_car->name,
-            'merk' => $M_car->merk,
-            'seat' => $M_car->seat,
+            'name'    => $M_car->name,
+            'merk'    => $M_car->merk,
+            'seat'    => $M_car->seat,
             'loading' => $M_car->loading,
-            'year' => $M_car->year,
+            'year'    => $M_car->year,
         ];  
     }
 }
